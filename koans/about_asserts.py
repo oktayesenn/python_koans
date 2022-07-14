0cecbd124f3ec4c8e0220b2d2e9a6a85a3b0b4bc
Oktay esen <oktayesenn@gmail.com>
2022-07-14 10:46:50 -0400

    Fixed Bugs

--- koans/about_asserts.py
+++ koans/about_asserts.py
@@ -14,7 +14,7 @@ class AboutAsserts(Koan):
         #
         #   http://bit.ly/about_asserts
 
-        self.assertTrue(False) # This should be True
+        self.assertTrue(True) # This should be True
 
     def test_assert_with_message(self):
         """
@@ -70,9 +70,8 @@ class AboutAsserts(Koan):
         #
         # See for yourself:
 
-        self.assertEqual(str, "naval".__class__) # It's str, not <type 'str'>
+        self.assertEqual(str, "navel".__class__) # It's str, not <type 'str'>
 
         # Need an illustration? More reading can be found here:
         #
         #   https://github.com/gregmalcolm/python_koans/wiki/Class-Attribute
-
